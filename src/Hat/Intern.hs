b0a5aed1ca090f85e1c0e7fe7c48a7aaec66ff9f
Bryan Richter <b@chreekat.net>
2026-08-27 14:06:32 +0300

    Clean up Intern comments

--- src/Hat/Intern.hs
+++ src/Hat/Intern.hs
@@ -1,5 +1,4 @@
--- | Value interning: collapse equal values to one shared heap representative,
--- so many equal copies cost a single allocation.
+-- | Value interning
 module Hat.Intern
     ( shareVals
     ) where
@@ -10,22 +9,31 @@ import Data.Map qualified as Map
 import System.Mem.Weak (Weak)
 import System.Mem.Weak qualified as Weak
 
--- | Return the interned representative of @x@: the first value passed through
--- the same table that was '==' to @x@, so all equal values collapse to one
--- shared heap object. On a miss (or once a previous representative has been
--- collected), @x@ itself becomes the representative. Each table entry refers
--- to its value through a 'Weak' whose finalizer drops the entry.
+-- | Given a value we want to share, return the shared/interned version of that
+-- value.
+--
+-- E.g. if two actions produce different instances of a, and a1==a2, this lets
+-- us GC one of them. This can save a lot of space if we are creating many
+-- instances of a that are all equal to each other!
 shareVals :: Ord a => IORef (Map a (Weak a)) -> a -> IO a
-shareVals intern x = do
+shareVals intern newA = do
     m <- readIORef intern
-    case Map.lookup x m of
+    case Map.lookup newA m of
+        -- We still have a Weak ptr keyed by newA
         Just weak -> do
             mv <- Weak.deRefWeak weak
             case mv of
-                Just v -> pure v
+                -- We still have the interned value! Return that.
+                -- Assuming nothing else is holding on to newA, it is now free
+                -- to be GCd, and we're left with just one copy of this value:
+                -- the old one.
+                Just oldV -> pure oldV
                 Nothing -> insert
         Nothing -> insert
   where
+    -- We never put this in the map, or we've caught it with its pants down (the
+    -- key is dead, but the finalizer hasn't fired yet). Insert or replace the
+    -- value.
     insert = do
-        weak <- Weak.mkWeak x x (Just (modifyIORef' intern (Map.delete x)))
-        x <$ modifyIORef' intern (Map.insert x weak)
+        weak <- Weak.mkWeak newA newA (Just (modifyIORef' intern (Map.delete newA)))
+        newA <$ modifyIORef' intern (Map.insert newA weak)
